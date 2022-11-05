3b994e09c77c01cd4a13b7eeedaecc0a06a8dea0
Arham Ali <99989233+ArhamAli970@users.noreply.github.com>
2022-11-05 21:55:16 +0530

    Add files via upload

---
+++ project1/bk.js
@@ -0,0 +1,12 @@
+function func(){  
+
+    document.getElementById('p56').style.display="block"
+    document.getElementById('p09').style.display="block" 
+  
+  } 
+   
+  function fun(){  
+    document.getElementById('p56').style.display="none" 
+    document.getElementById('p09').style.display="none" 
+  
+  }
